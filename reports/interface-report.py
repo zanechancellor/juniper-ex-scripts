f277c7d9c457fa15f9e2d1f896aa00d9e2392989
Zane Chancellor <61860198+zanechancellor@users.noreply.github.com>
2025-09-20 19:54:11 -0600

    Updated interface-report.py
    - Added Comments to code.
    - Added variable to get hostname and add to each row of the csv.

--- reports/interface-report.py
+++ reports/interface-report.py
@@ -7,6 +7,7 @@ from jnpr.junos.factory.factory_loader import FactoryLoader
 import yaml
 import csv
 
+# Define the tables for juniper pyez
 juniperTables="""
 ---
 InterfaceTable:
@@ -25,19 +26,30 @@ InterfaceView:
     flap: { interface-flapped/@seconds : int }
 
 """
-
+# Load the tables into pyez
 globals().update(FactoryLoader().load(yaml.load(juniperTables, Loader=yaml.FullLoader)))
 
+# Gather login info
 hostname=input("IP/hostname: ")
 username=input("Username: ")
 password=getpass()
 
+# Open device connection
 with Device(host=hostname, user=username, passwd=password) as dev:
+    # Define table and gather info
     interfaces=InterfaceTable(dev)
     interfaces.get()
 
+    # Gather Device hostname for csv
+    switch=dev.facts['hostname']
+
+    # Create and write CSV file
     with open("interface-report.csv", "w", newline="") as csvfile:
       writer = csv.writer(csvfile)
-      writer.writerow(["interface","adminStatus","physicalStatus","lastStatusChangeDays"])
+
+      # Write headers
+      writer.writerow(["switch","interface","adminStatus","physicalStatus","lastStatusChangeDays"])
+      
+      # Create a new row with the interface info
       for interface in interfaces:
-          writer.writerow([interface.name, interface.adminStatus, interface.status, (interface.flap/60/60/24)])
+          writer.writerow([switch,interface.name, interface.adminStatus, interface.status, (interface.flap/60/60/24)])
