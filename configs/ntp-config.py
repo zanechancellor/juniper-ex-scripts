be93a92cedc4c7478d01c29b85db4449216a81d1
Zane Chancellor <61860198+zanechancellor@users.noreply.github.com>
2025-09-25 19:02:46 -0600

    Added ntp-config.py and ntp.conf

---
+++ configs/ntp-config.py
@@ -0,0 +1,35 @@
+from getpass import getpass
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config
+from jnpr.junos.exception import ConnectUnknownHostError, ConnectTimeoutError, ConnectRefusedError, ConnectAuthError
+
+conf_file = 'configs/ntp.conf'
+config_vars = {
+    'ntpserver': [{'address':'10.0.0.0','key':'101'}, {'address':'10.0.0.1'}],
+    'ntpkey': [{'id':'101', 'type':'sha256', 'value':'Lab1234!!'}],
+    'trustedkeys': ['101']
+}
+
+# Gather login info
+hostname=input("IP/hostname: ")
+username=input("Username: ")
+password=getpass()
+
+try:
+    with Device(host=hostname, user=username, passwd=password) as dev:
+        with Config(dev, mode='exclusive') as cu:
+            cu.load(template_path=conf_file, template_vars=config_vars)
+            cu.pdiff()
+            if input('Type "yes" to commit the config, otherwise discard. ').lower() =='yes':
+                cu.commit()
+                print('Commit Complete')
+            else:
+                print('Config not commited')
+except ConnectUnknownHostError:
+    print(f'Unable to {hostname}')
+except ConnectTimeoutError:
+    print(f'Connection timed out to {hostname}')
+except ConnectRefusedError:
+    print(f'{hostname} refused connection')
+except ConnectAuthError:
+    print(f'Username or password incorrect on {hostname}')
